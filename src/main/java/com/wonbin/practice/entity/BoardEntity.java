89a6ff5b9ded38cd4996ed8db24cc658fdf111be
Jo-wonbin <jwb7214@gmail.com>
2024-01-07 12:33:30 +0900

    Feat : entity 파일 추가

---
+++ src/main/java/com/wonbin/practice/entity/BoardEntity.java
@@ -0,0 +1,45 @@
+package com.wonbin.practice.entity;
+
+import com.wonbin.practice.dto.BoardDto;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "board_table")
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardEntity extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String boardWriter;
+
+    @Column // 크기  255, null 가능
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+
+    public static BoardEntity toSaveEntity(BoardDto boardDto){
+        BoardEntity boardEntity = BoardEntity.builder()
+                .id(boardDto.getId())
+                .boardWriter(boardDto.getBoardWriter())
+                .boardPass(boardDto.getBoardPass())
+                .boardContents(boardDto.getBoardContents())
+                .boardHits(0)
+                .build();
+        return boardEntity;
+    }
+
+}
--- src/main/java/com/wonbin/practice/entity/UserEntity.java
+++ src/main/java/com/wonbin/practice/entity/UserEntity.java
@@ -2,12 +2,13 @@ package com.wonbin.practice.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class UserEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
 }
