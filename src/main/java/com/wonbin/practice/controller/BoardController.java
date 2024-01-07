5b60a7af93f6e355233c2ac2ff95c1517be512ec
Jo-wonbin <jwb7214@gmail.com>
2024-01-07 12:35:21 +0900

    Modify : controller 내용 추가

--- src/main/java/com/wonbin/practice/controller/BoardController.java
+++ src/main/java/com/wonbin/practice/controller/BoardController.java
@@ -1,17 +1,31 @@
 package com.wonbin.practice.controller;
 
+import com.wonbin.practice.dto.BoardDto;
+import com.wonbin.practice.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+
+    private final BoardService boardService;
 
     @GetMapping("/save")
     public String saveForm() {
         return "save";
     }
 
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDto boardDto){
+        System.out.println("boardDTO = " + boardDto);
+        boardService.save(boardDto);
+        return "index";
+    }
 
 }
