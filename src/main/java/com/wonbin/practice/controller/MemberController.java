ec541372bc351cb139b5cdbcd358b53e859337de
Jo-wonbin <jwb7214@gmail.com>
2024-01-10 14:32:41 +0900

    Feat : member 로그인, 회원가입 기능 추가

---
+++ src/main/java/com/wonbin/practice/controller/MemberController.java
@@ -0,0 +1,128 @@
+package com.wonbin.practice.controller;
+
+import com.wonbin.practice.dto.MemberDto;
+import com.wonbin.practice.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@Tag(name = "Member")
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Operation(summary = "회원 전체 목록 조회")
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<MemberDto> memberDtoList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있을 때 model 사용
+        // memberList 란 이름에 리스트를 넣음.
+        model.addAttribute("memberList", memberDtoList);
+        return "listMember";
+    }
+
+    @Operation(summary = "회원가입 페이지 출력 요청")
+    @GetMapping("/save")
+    public String saveForm() {
+        System.out.println("MemberController.saveForm");
+        return "saveMember";
+    }
+
+    @Operation(summary = "회원가입 정보 DB 저장")
+    @PostMapping("/save")
+    public String save(@ModelAttribute MemberDto memberDto
+    ) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDto = " + memberDto.toString());
+        memberService.save(memberDto);
+        return "login";
+    }
+
+    @Operation(summary = "로그인 창 실행")
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @Operation(summary = "회원 정보를 받아 로그인 여부 체크 후 로그인")
+    @PostMapping("/login")
+    public String login(@ModelAttribute MemberDto memberDto, HttpSession httpSession) {
+        System.out.println("MemberController.login");
+        MemberDto loginResult = memberService.login(memberDto);
+        if (loginResult != null) {
+            // 로그인 성공 후 세션 부여
+            httpSession.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            return "login";
+        }
+    }
+
+    @Operation(summary = "특정 회원 정보를 상세 조회")
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        System.out.println("MemberController.findById");
+        System.out.println("id = " + id);
+
+        MemberDto memberDto = memberService.findById(id);
+        model.addAttribute("member", memberDto);
+
+        return "detailMember";
+    }
+
+    @Operation(summary = "회원 정보 수정 창 이동")
+    @GetMapping("/update")
+    public String updateForm(HttpSession httpSession, Model model) {
+        System.out.println("MemberController.updateForm");
+        String myEmail = (String) httpSession.getAttribute("loginEmail");
+        MemberDto memberDto = memberService.updateForm(myEmail);
+
+        // 세션 이메일과 같으면 updateForm에 담길 정보를 updateMember에 담아 전송
+        model.addAttribute("updateMember", memberDto);
+        return "updateMember";
+    }
+
+    @Operation(summary = "회원 정보 수정")
+    @PostMapping("/update")
+    public String update(@ModelAttribute MemberDto memberDto) {
+        memberService.update(memberDto);
+
+        return "redirect:/member/" + memberDto.getId();
+    }
+
+    @Operation(summary = "회원 정보 삭제")
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        memberService.delete(id);
+
+        return "redirect:/member/";
+    }
+
+    @Operation(summary = "로그아웃 후 세션 삭제")
+    @GetMapping("/logout")
+    public String logout(HttpSession httpSession) {
+        httpSession.invalidate();
+        return "index";
+    }
+
+    @Operation(summary = "이메일 중복 체크")
+    @PostMapping("/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
+        System.out.println("memberEmail = " + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        if (checkResult != null) {
+            return "ok";
+        } else {
+            return "no";
+        }
+    }
+}
---
+++ src/main/java/com/wonbin/practice/service/MemberService.java
@@ -0,0 +1,93 @@
+package com.wonbin.practice.service;
+
+import com.wonbin.practice.dto.MemberDto;
+import com.wonbin.practice.entity.MemberEntity;
+import com.wonbin.practice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void save(MemberDto memberDto) {
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDto);
+        memberRepository.save(memberEntity);
+    }
+
+    public MemberDto login(MemberDto memberDto) {
+        /*
+            1. 회원이 입력한 이메일로 DB에서 조회를 함.
+            2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판단
+         */
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDto.getMemberEmail());
+        if (byMemberEmail.isPresent()) {
+            // 조회 결과가 있으면
+            MemberEntity memberEntity = byMemberEmail.get();
+            if (memberEntity.getMemberPassword().equals(memberDto.getMemberPassword())) {
+                // 비밀번호 일치
+                MemberDto dto = MemberDto.toMemberDto(memberEntity);
+                return dto;
+            } else {
+                // 로그인 실패
+                return null;
+            }
+        } else
+            // 조회 결과가 없으면 널
+            return null;
+    }
+
+    public List<MemberDto> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+
+        List<MemberDto> memberDtoList = new ArrayList<>();
+        for (MemberEntity memberEntity : memberEntityList) {
+            memberDtoList.add(MemberDto.toMemberDto(memberEntity));
+        }
+        return memberDtoList;
+    }
+
+    public MemberDto findById(Long id) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
+        if (optionalMemberEntity.isPresent()) {
+            MemberEntity memberEntity = optionalMemberEntity.get();
+            MemberDto memberDto = MemberDto.toMemberDto(memberEntity);
+
+            return memberDto;
+        } else {
+            return null;
+        }
+    }
+
+    public MemberDto updateForm(String myEmail) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(myEmail);
+        if (optionalMemberEntity.isPresent()) {
+            return MemberDto.toMemberDto(optionalMemberEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public void update(MemberDto memberDto) {
+        memberRepository.save(MemberEntity.toUpdateMemberEntity(memberDto));
+    }
+
+    public void delete(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    public String emailCheck(String memberEmail) {
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberEmail);
+        if (byMemberEmail.isPresent()) {
+            return null;
+        } else {
+            return "ok";
+        }
+    }
+}
