fd383616f3ca84e8219dc06da1b7e96810c0786c
Jo-wonbin <jwb7214@gmail.com>
2024-01-11 19:50:42 +0900

    Modify : ddl-auto가 create일 경우만 작동하도록 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO province_table(name)
-VALUES ('인천시'),('서울시'), ('부산시');
+INSERT INTO province_table(id, name)
+VALUES (1,'인천시'),(2, '서울시'), (3, '부산시');
 
 INSERT INTO district_table(province_id ,name)
 VALUES (1, '동구'), (1, '서구'),(1, '남구'),
