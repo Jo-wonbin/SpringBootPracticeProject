f02a5504ed3008fd075f2a2adf7c4a3632403925
Jo-wonbin <jwb7214@gmail.com>
2024-01-12 20:12:40 +0900

    Feat : 쿼리메소드 추가

--- src/main/java/com/wonbin/practice/repository/DistrictRepository.java
+++ src/main/java/com/wonbin/practice/repository/DistrictRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface DistrictRepository extends JpaRepository<DistrictEntity, Long> {
     List<DistrictEntity> findAllByProvinceEntity(ProvinceEntity provinceEntity);
+
 }
