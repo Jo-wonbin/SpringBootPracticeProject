2bfc8071d77b87aa1088821169920347cd60bf6e
Jo-wonbin <jwb7214@gmail.com>
2024-01-17 13:47:12 +0900

    Feat : 광역시, 지역구 관련 페이징 조회 로직 추가

---
+++ src/main/java/com/wonbin/practice/specification/BoardSpecification.java
@@ -0,0 +1,15 @@
+package com.wonbin.practice.specification;
+
+import com.wonbin.practice.entity.BoardEntity;
+import org.springframework.data.jpa.domain.Specification;
+
+public class BoardSpecification {
+
+    public static Specification<BoardEntity> hasProvinceId(Long provinceId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("provinceId"), provinceId);
+    }
+
+    public static Specification<BoardEntity> hasDistrictId(Long districtId) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("districtId"), districtId);
+    }
+}
