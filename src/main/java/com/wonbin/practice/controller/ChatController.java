09a29ddd8f6c2485ab9c13207dc52ecf060f81c3
Jo-wonbin <jwb7214@gmail.com>
2024-01-23 00:51:05 +0900

    Feat : 채팅 리스트 조회 로직 추가

--- src/main/java/com/wonbin/practice/controller/ChatController.java
+++ src/main/java/com/wonbin/practice/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.wonbin.practice.controller;
 
-import com.wonbin.practice.dto.ChatMessageDto;
+import com.wonbin.practice.dto.chat.ChatListDto;
+import com.wonbin.practice.dto.chat.ChatMessageDto;
 import com.wonbin.practice.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,10 +10,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -50,4 +48,15 @@ public class ChatController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/chatList")
+    public ResponseEntity<ChatListDto> getChatList(@RequestBody String memberEmail) {
+        ChatListDto chatListDto = chatService.findChatList(memberEmail);
+        if (chatListDto != null) {
+            return new ResponseEntity<>(chatListDto, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
