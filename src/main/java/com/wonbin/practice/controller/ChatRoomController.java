6f086fdce4b748552a9868f04d68085c802f87d3
Jo-wonbin <jwb7214@gmail.com>
2024-01-23 20:29:23 +0900

    Refactor : 파일 이름 변경

--- src/main/java/com/wonbin/practice/controller/ChatRoomController.java
+++ src/main/java/com/wonbin/practice/controller/ChatRoomController.java
@@ -3,6 +3,7 @@ package com.wonbin.practice.controller;
 import com.wonbin.practice.dto.chat.ChatListDto;
 import com.wonbin.practice.dto.chat.ChatMessageDto;
 import com.wonbin.practice.service.ChatService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,15 +24,17 @@ public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatService chatService;
 
+    /*
+        "/app/chat" 요청으로 온 메세지
+     */
     @MessageMapping("/chat")
-    public void processMessage(@Payload ChatMessageDto chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+    public void processMessage(@Payload ChatMessageDto chatMessage, SimpMessageHeaderAccessor headerAccessor) throws Exception {
 
 
         chatMessage.setMessageCreatedTime(new Date());
         chatService.save(chatMessage);
-        System.out.println(chatMessage.toString());
 
-        // Broadcast the message to /topic/messages
+        // 클라이언트로 메세지 보내기 /topic/messages
         messagingTemplate.convertAndSend("/topic/messages", chatMessage);
     }
 
@@ -50,8 +53,16 @@ public class ChatController {
     }
 
     @GetMapping("/chatList")
-    public ResponseEntity<ChatListDto> getChatList(@RequestBody String memberEmail) {
-        ChatListDto chatListDto = chatService.findChatList(memberEmail);
+    public ResponseEntity<ChatListDto> getChatList(HttpSession session) {
+        String loginEmail = (String) session.getAttribute("loginEmail");
+
+        if (loginEmail == null) {
+            // 세션에서 로그인 이메일이 없는 경우 처리
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        ChatListDto chatListDto = chatService.findChatList(loginEmail);
+
         if (chatListDto != null) {
             return new ResponseEntity<>(chatListDto, HttpStatus.OK);
         } else {
