2e54eae460cebf4270ed818f29d24520584c5917
Jo-wonbin <jwb7214@gmail.com>
2024-02-15 15:21:53 +0900

    Modify  : 이미지 저장 경로 변경

--- src/main/java/com/wonbin/practice/service/BoardService.java
+++ src/main/java/com/wonbin/practice/service/BoardService.java
@@ -18,6 +18,9 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
@@ -43,6 +46,9 @@ public class BoardService {
     private final DistrictRepository districtRepository;
 
     private final String savePath = "C:/Users/user/Desktop/SpringBoot/image/";
+    //    private final String savePath = "/etc/picture/";
+    private final int scaledWidth = 300;
+    private final int scaledHeight = 300;
 
     public Long save(BoardDto boardDto, String email) throws IOException {
         /*
@@ -70,7 +76,6 @@ public class BoardService {
         }
         // 파일 첨부 여부에 따라 로직 분리
         if (boardDto.getBoardFile().isEmpty() || boardDto.getBoardFile().get(0).getSize() == 0) {
-            System.out.println("파일이 비어있습니다.");
             // 첨부 파일 없을 때
             BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDto);
             BoardEntity save = boardRepository.save(boardEntity);
@@ -80,7 +85,6 @@ public class BoardService {
                 return 0L;
             }
         } else {
-            System.out.println("파일이 비지 않았습니다.");
             BoardEntity boardEntity = BoardEntity.toSaveFileEntity(boardDto);
             Long saveId = boardRepository.save(boardEntity).getId();
             Optional<BoardEntity> board = boardRepository.findById(saveId);
@@ -91,10 +95,37 @@ public class BoardService {
                 String originalFilename = boardFile.getOriginalFilename(); // 2. 파일의 이름을 가져옴
                 // 유저 id도 저장하여 100% 겹치지 않은 파일
                 String storedFilename = System.currentTimeMillis() + "_" + originalFilename; // 3. 서버 저장용 이름을 만든다. // 내사진.jpg => 8826371246_내사진.jpg
-                String savePath = "C:/Users/user/Desktop/SpringBoot/image/" + storedFilename; // 4. 저장 경로 설정
-                boardFile.transferTo(new File(savePath)); // 5. 해당 경로에 파일 저장
-                BoardFileEntity boardFileEntity = BoardFileEntity.toBoardFileEntity(board.get(), originalFilename, storedFilename); // 6. board_table에 해당 데이터 save 처리
-                fileSave = boardFileRepository.save(boardFileEntity);// 7. board_file_table에 해당 데이터 save 처리
+                String picturePath = savePath + storedFilename; // 4. 저장 경로 설정
+
+                // 이미지 파일을 읽어옴
+                BufferedImage originalImage = ImageIO.read(boardFile.getInputStream());
+                // 이미지 크기를 조정
+                int width = originalImage.getWidth();
+                int height = originalImage.getHeight();
+                if (width > scaledWidth || height > scaledHeight) {
+                    double scale = Math.min((double) scaledWidth / width, (double) scaledHeight / height);
+                    int nowWidth = (int) (width * scale);
+                    int nowHeight = (int) (height * scale);
+
+                    // 이미지를 축소
+                    Image scaledImage = originalImage.getScaledInstance(nowWidth, nowHeight, Image.SCALE_SMOOTH);
+                    BufferedImage resizedImage = new BufferedImage(nowWidth, nowHeight, BufferedImage.TYPE_INT_RGB);
+                    Graphics2D g = resizedImage.createGraphics();
+                    g.drawImage(scaledImage, 0, 0, null);
+                    g.dispose();
+
+                    // 축소된 이미지를 저장
+                    ImageIO.write(resizedImage, "jpg", new File(picturePath));
+                } else {
+                    // 크기가 축소할 필요가 없는 경우 원본 이미지를 저장
+                    boardFile.transferTo(new File(picturePath));
+                }
+
+                // 6. board_table에 해당 데이터 save 처리
+                BoardFileEntity boardFileEntity = BoardFileEntity.toBoardFileEntity(board.get(), originalFilename, storedFilename);
+
+                // 7. board_file_table에 해당 데이터 save 처리
+                fileSave = boardFileRepository.save(boardFileEntity);
             }
             if (fileSave != null) {
                 return 1L;
@@ -160,8 +191,32 @@ public class BoardService {
                         String originalFilename = boardFile.getOriginalFilename(); // 2. 파일의 이름을 가져옴
                         // 유저 id도 저장하여 100% 겹치지 않은 파일
                         String storedFilename = System.currentTimeMillis() + "_" + originalFilename; // 3. 서버 저장용 이름을 만든다. // 내사진.jpg => 8826371246_내사진.jpg
-                        String savePath = "C:/Users/user/Desktop/SpringBoot/image/" + storedFilename; // 4. 저장 경로 설정
-                        boardFile.transferTo(new File(savePath)); // 5. 해당 경로에 파일 저장
+                        String picturePath = savePath + storedFilename; // 4. 저장 경로 설정
+
+                        // 이미지 파일을 읽어옴
+                        BufferedImage originalImage = ImageIO.read(boardFile.getInputStream());
+                        // 이미지 크기를 조정
+                        int width = originalImage.getWidth();
+                        int height = originalImage.getHeight();
+                        if (width > scaledWidth || height > scaledHeight) {
+                            double scale = Math.min((double) scaledWidth / width, (double) scaledHeight / height);
+                            int nowWidth = (int) (width * scale);
+                            int nowHeight = (int) (height * scale);
+
+                            // 이미지를 축소
+                            Image scaledImage = originalImage.getScaledInstance(nowWidth, nowHeight, Image.SCALE_SMOOTH);
+                            BufferedImage resizedImage = new BufferedImage(nowWidth, nowHeight, BufferedImage.TYPE_INT_RGB);
+                            Graphics2D g = resizedImage.createGraphics();
+                            g.drawImage(scaledImage, 0, 0, null);
+                            g.dispose();
+
+                            // 축소된 이미지를 저장
+                            ImageIO.write(resizedImage, "jpg", new File(picturePath));
+                        } else {
+                            // 크기가 축소할 필요가 없는 경우 원본 이미지를 저장
+                            boardFile.transferTo(new File(picturePath));
+                        }
+
                         BoardFileEntity boardFileEntity = BoardFileEntity.toBoardFileEntity(boardEntity, originalFilename, storedFilename); // 6. board_table에 해당 데이터 save 처리
                         boardFileRepository.save(boardFileEntity);// 7. board_file_table에 해당 데이터 save 처리
                     }
