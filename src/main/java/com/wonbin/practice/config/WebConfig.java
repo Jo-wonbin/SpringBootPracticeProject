640c81d251b8f9e78d125d3380b75293cd92152d
Jo-wonbin <jwb7214@gmail.com>
2024-02-19 23:40:28 +0900

    Modify : CORS 설정 변경

--- src/main/java/com/wonbin/practice/config/WebConfig.java
+++ src/main/java/com/wonbin/practice/config/WebConfig.java
@@ -1,22 +1,36 @@
 package com.wonbin.practice.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${spring.cors.allowed-origins}") // 변경된 부분
-    private String[] allowedOrigins;
+    private final List<String> allowedOrigins = Arrays.asList(
+            "http://localhost",
+            "https://localhost",
+            "http://localhost:8080",
+            "https://localhost:8080",
+            "ws://localhost",
+            "wss://localhost",
+            "ws://localhost:8080",
+            "wss://localhost:8080",
+            "http://34.64.195.226",
+            "https://34.64.195.226",
+            "http://34.64.195.226:8080",
+            "https://34.64.195.226:8080",
+            "ws://34.64.195.226",
+            "wss://34.64.195.226",
+            "ws://34.64.195.226:8080",
+            "wss://34.64.195.226:8080"
+    );
 
-    @Value("${spring.resource-path}") // 변경된 부분
-    private String resourcePath;
-
-    @Value("${spring.save-path}") // 변경된 부분
-    private String savePath;
+    private final String resourcePath = "/upload/**";
+    private final String savePath = "file:///etc/picture/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -26,7 +40,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
